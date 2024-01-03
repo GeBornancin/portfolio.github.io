f8083e35d6cf362a2400978df5e6f902905a5bfb
Geovane Bornancin <95493767+GeBornancin@users.noreply.github.com>
2024-01-03 18:47:33 -0300

    Add new files and make changes to existing files

---
+++ js/script.js
@@ -0,0 +1,34 @@
+function copyToClipboard() {
+    // Copia o texto para a área de transferência
+    const email = "gebornancin@gmail.com";
+    navigator.clipboard.writeText(email);
+
+    // Exibe a mensagem de confirmação
+    const confirmationMessage = document.createElement("div");
+    confirmationMessage.innerText =
+      "E-mail copiado para a área de transferência!";
+    confirmationMessage.style.position = "fixed";
+    confirmationMessage.style.bottom = "20px";
+    confirmationMessage.style.left = "50%";
+    confirmationMessage.style.transform = "translateX(-50%)";
+    confirmationMessage.style.padding = "10px";
+    confirmationMessage.style.backgroundColor = "black";
+    confirmationMessage.style.color = "white";
+    confirmationMessage.style.borderRadius = "5px";
+    confirmationMessage.style.opacity = "0.8";
+    confirmationMessage.style.zIndex = "9999";
+    document.body.appendChild(confirmationMessage);
+
+    // Remove a mensagem de confirmação após 2 segundos
+    setTimeout(() => {
+      confirmationMessage.remove();
+    }, 2000);
+  }
+
+  $(document).ready(function(){
+    $(".sportdatalink video").hover(function(){
+      $(".video-hover-text").addClass("show");
+    }, function(){
+      $(".video-hover-text").removeClass("show");
+    });
+  });
